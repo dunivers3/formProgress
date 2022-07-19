454dd6a1a5b67d7b9500a3a9c55787608bb65d0d
Duncan <wangari.dun@gmail.com>
2022-07-19 21:32:53 +0300

    Updates the search bar from toggle to add

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,8 @@ const progressEl = document.getElementById("progress")
 const backEl = document.getElementById("back")
 const nextEl = document.getElementById("next")
 const circles = document.querySelectorAll(".circle")
+
+//search constants
 const search = document.querySelector(".search")
 const searchButton = document.querySelector(".searchBtn")
 const searchInput = document.querySelector(".input")
@@ -9,46 +11,46 @@ const searchInput = document.querySelector(".input")
 let activeEl = 1
 
 nextEl.addEventListener("click", () => {
-	activeEl++;
-	if (activeEl > circles.length) {
-		activeEl = circles.length
-	}
-	updateProgress()
+    activeEl++;
+    if (activeEl > circles.length) {
+        activeEl = circles.length
+    }
+    updateProgress()
 })
 
 backEl.addEventListener("click", () => {
-	activeEl--
-	if (activeEl < 1) {
-		activeEl = 1
-	}
-	updateProgress()
+    activeEl--
+    if (activeEl < 1) {
+        activeEl = 1
+    }
+    updateProgress()
 })
 
 //function that adds and removes active classess on the dive, and calculates the length of the progress bar on the DOM
 function updateProgress() {
-	circles.forEach((circles, idx) => {
-		if (idx < activeEl) {
-			circles.classList.add("active")
-		} else {
-			circles.classList.remove("active")
-		}
-	})
+    circles.forEach((circle, idx) => {
+        if (idx < activeEl) {
+            circle.classList.add("active")
+        } else {
+            circle.classList.remove("active")
+        }
+    })
 
-	const activeCircles = document.querySelectorAll(".active")
-	progressEl.style.width = (activeCircles.length - 1) / (circles.length - 1) * 100 + `%`
+    const activeCircles = document.querySelectorAll(".active")
+    progressEl.style.width = (activeCircles.length - 1) / (circles.length - 1) * 100 + `%`
 
-	if (activeEl === 1) {
-		backEl.disabled = true
-	} else if (activeEl === circles.length) {
-		nextEl.disabled = true
-	} else {
-		nextEl.disabled = false
-		backEl.disabled = false
-	}
+    if (activeEl === 1) {
+        backEl.disabled = true
+    } else if (activeEl === circles.length) {
+        nextEl.disabled = true
+    } else {
+        nextEl.disabled = false
+        backEl.disabled = false
+    }
 
 }
 
 searchButton.addEventListener("click", () => {
-	search.classList.toggle("active")
-	searchInput.focus()
+    search.classList.add("searchActive")
+    searchInput.focus()
 })
